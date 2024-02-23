81614428d59271124a95dfe7749db4441f01625b
Geebee-create <156425632+Geebee-create@users.noreply.github.com>
2024-02-23 16:48:17 +0000

    More done
    More done

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import getRandomQuote from './quote.js';
 import './App.css';
 
 const App = () => {
-  const [activityInput, setActivityInput] = useState("")
+  const [quote, setQuote] = useState('');
+  setDates([...Date, { day: parseInt(dayInput), month: parseInt(monthInput), year: parseInt(yearInput) }])
+ const [activityInput, setActivityInput] = useState("")
 
+ const handleGenerateQuote = () => {
+  const randomQuote = getRandomQuote();
+  setQuote(randomQuote);
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -14,15 +19,21 @@ const App = () => {
     setActivityInput("")
     
   }
+}
 
   return (
     <div>
       <h1>To Do App</h1>
+      <button onClick={handleGenerateQuote}>Generate Positive Quote</button>
+      {quote && <p>{quote}</p>}
       <Form 
         activityInput={activityInput} 
         setActivityInput={setActivityInput}
         handleSubmit={handleSubmit}
       />
+       {Date.map((Date) => {
+        return <Date day={Date.day} month={Date.month} year={Date.year} activityInput={Date.activityInput}/>
+      })}
     </div>
   )
 }
--- src/Date.js
+++ src/Date.js
@@ -1,9 +1,12 @@
-const Date = ({ day, month, year }) => {
+import Form from './Form.js';
+
+const Date = ({day, month, year, activityInput}) => {
     return (
        <div>
         <p>{day}</p>
         <p>{month}</p>
         <p>{year}</p>
+        <p>{activityInput}</p>
         <button>delete</button>
        </div> 
     )
